3d063940e586c1a0be0871fcfc8ab137c6a59d31
matan <rahavmatan@gmail.com>
2017-07-07 18:11:28 +0300

    server.js is now an api server for fetching phones

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var express= require ('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.use(express.static(__dirname ));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('index.html')
+var router = express.Router();
+
+
+router.route('/phones')
+.get(function(req,res){
+  res.json( require('./src/phones/phones.json'));
 })
+
+router.route('/phones/:phoneName')
+.get(function(req,res){
+  res.json( require(`./src/phones/${req.params.phoneName}.json`))
+})
+
+app.use('/api', router);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 
-import App from './components/app';
 import Home from './components/home';
 import reducers from './reducers';
 
