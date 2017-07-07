69327a0ea8a6ca206173b4e8c0255f19b8bd378d
matan <rahavmatan@gmail.com>
2017-07-07 12:30:06 +0300

    fixed webpack.config to work with v2.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,37 @@
+const path = require('path');
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: './src/index.js',
   output: {
     path: __dirname,
-    publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
   }
-};
+}
+
+// module.exports = {
+//   entry: [
+//     './src/index.js'
+//   ],
+//   output: {
+//     path: __dirname,
+//     publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     loaders: [{
+//       exclude: /node_modules/,
+//       loader: 'babel',
+//       query: {
+//         presets: ['react', 'es2015', 'stage-1']
+//       }
+//     }]
+//   },
+//   resolve: {
+//     extensions: ['*', '.js', '.jsx']
+//   }
+// };
